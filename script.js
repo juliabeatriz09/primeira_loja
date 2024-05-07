7a60b9566311dd1b3019e5489243397ae8665348
juliabeatriz09 <159266648+juliabeatriz09@users.noreply.github.com>
2024-05-07 00:04:39 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ function ola() {
     nome = prompt(" nome ");
     alert(nome);
 }
+
+function troca_item() {
+    var estojo= document.getElementById('esto');
+
+    if(esto.src.match("estojoo.jpeg")){
+        estojo.src ="estojo.jpeg";
+    } else {
+        esto.scr = "estojoo.jpeg"
+    }
+}
