12f325498462320ba2cc18a02b5164c5067697a5
juliabeatriz09 <159266648+juliabeatriz09@users.noreply.github.com>
2024-05-15 17:04:18 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ function ola() {
     alert(nome);
 }
 
-function troca_item() {
-    var coraçao= document.getElementById('like');
+    function troca_item() {
+      var coraçao= document.getElementById('like');
+      var coraçaoo= document.getElementById('curtir');
 
-    if(like.src.match('coraçao.png')){
-        curtir.src ="coraçaor.jpeg";
+    if(like.src.match("coraçao.png")){
+        curtir.src ="coraçaoo.png";
     } else {
         like.scr = "coraçao.png";
     }
+
 }
